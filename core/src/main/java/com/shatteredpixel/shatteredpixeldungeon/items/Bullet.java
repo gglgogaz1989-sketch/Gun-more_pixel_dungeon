9388362be0ae60f5a5ddd7c0a6f3c5de4e670e75
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 17:29:51 +0200

    Update Bullet.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
@@ -1,1 +1,18 @@
+package com.shatteredpixel.shatteredpixeldungeon.items;
 
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+
+public class Bullet extends Item {
+    {
+        // Имя и описание
+        name = "Золотой патрон";
+        // Номер иконки в атласе (нужно посчитать точный индекс в твоем файле)
+        image = 127; 
+        stackable = true;
+    }
+
+    @Override
+    public String info() {
+        return "Тяжелый золотой патрон. Выглядит слишком ценным, чтобы просто им выстрелить, но выбора нет.";
+    }
+}
