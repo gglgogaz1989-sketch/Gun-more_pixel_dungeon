7a72cebd2c24c55f73db04a49229297299dbb97b
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 20:21:09 +0200

    Update NecroRat.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/NecroRat.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/NecroRat.java
@@ -4,29 +4,35 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Charm;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.NecroRatSprite;
+import com.shatteredpixel.shatteredpixeldungeon.Badges; 
 import com.watabou.utils.Random;
 
 public class NecroRat extends Rat {
 
-	{
-		// Привязываем спрайт
-		spriteClass = NecroRatSprite.class;
+    {
+        spriteClass = NecroRatSprite.class;
+        HP = HT = 16;
+        defenseSkill = 4;
+        EXP = 3;
+    }
 
-		HP = HT = 16;
-		defenseSkill = 4;
-		EXP = 3;
-	}
+    @Override
+    public int attackProc( Char enemy, int damage ) {
+        int actualDamage = super.attackProc( enemy, damage );
+        
+        // Шанс 50% наложить существующее в игре Очарование
+        if (actualDamage > 0 && Random.Int( 2 ) == 0) {
+            Buff.affect( enemy, Charm.class ).extend( Random.NormalFloat(3, 5) );
+        }
+        
+        return actualDamage;
+    }
 
-	@Override
-	public int attackProc( Char enemy, int damage ) {
-		int actualDamage = super.attackProc( enemy, damage );
-		
-		// Если урон > 0, с шансом 50% накладываем Charm
-		if (actualDamage > 0 && Random.Int( 2 ) == 0) {
-			// Накладываем бафф Charm на врага. Источник — эта крыса (this)
-			Buff.affect( enemy, Charm.class ).set( Random.NormalFloat(3, 5) );
-		}
-		
-		return actualDamage;
-	}
+    @Override
+    public void die( Object cause ) {
+        super.die( cause );
+        
+        // Вызываем проверку достижения при смерти
+        Badges.validateNecroRatKilled();
+    }
 }
