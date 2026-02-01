5535c7d3f80c4ef223d66c487f2f64801fcf3e45
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 20:18:14 +0200

    Update NecroRatSprite.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/NecroRatSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/NecroRatSprite.java
@@ -1,18 +1,34 @@
 package com.shatteredpixel.shatteredpixeldungeon.sprites;
 
 import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.watabou.noosa.TextureFilm;
 
-public class NecroRatSprite extends RatSprite {
-    public NecroRatSprite() {
-        super();
-        // Берем твой файл с розовоглазой крысой
-        texture = Assets.RAT; 
-    }
-
-    @Override
-    public void update() {
-        super.update();
-        // offset(0, 3) заставляет игру брать 4-й ряд в картинке (розовые глаза)
-        offset( 0, 3 ); 
-    }
+public class NecroRatSprite extends MobSprite {
+	
+	public NecroRatSprite() {
+		super();
+		
+		// Если Assets.Sprites.RAT выдаст ошибку, поменяй на Assets.RAT
+		texture( Assets.Sprites.RAT );
+		
+		TextureFilm frames = new TextureFilm( texture, 16, 15 );
+		
+		// Кадры покоя (48, 48, 48, 49)
+		idle = new Animation( 2, true );
+		idle.frames( frames, 48, 48, 48, 49 );
+		
+		// Кадры бега (54, 55, 56, 57, 58)
+		run = new Animation( 10, true );
+		run.frames( frames, 54, 55, 56, 57, 58 );
+		
+		// Кадры атаки (50, 51, 52, 53, 48)
+		attack = new Animation( 15, false );
+		attack.frames( frames, 50, 51, 52, 53, 48 );
+		
+		// Кадры смерти (59, 60, 61, 62)
+		die = new Animation( 10, false );
+		die.frames( frames, 59, 60, 61, 62 );
+		
+		play( idle );
+	}
 }
