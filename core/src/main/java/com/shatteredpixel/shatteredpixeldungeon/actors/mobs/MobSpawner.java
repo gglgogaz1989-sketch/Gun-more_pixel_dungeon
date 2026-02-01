d9568b945042724b3fd44f3f1d2d3bea52b7f646
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 17:27:27 +0200

    Update MobSpawner.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/MobSpawner.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/MobSpawner.java
@@ -241,12 +241,12 @@ public class MobSpawner extends Actor {
 
 	//switches out regular mobs for their alt versions when appropriate
 	private static void swapMobAlts(ArrayList<Class<?extends Mob>> rotation) {
-		float altChance = 1 / 50f * RatSkull.exoticChanceMultiplier();
+		float altChance = 1 / 20f * RatSkull.exoticChanceMultiplier();
 		for (int i = 0; i < rotation.size(); i++) {
 			if (Random.Float() < altChance) {
 				Class<? extends Mob> cl = rotation.get(i);
 				if (cl == Rat.class)                cl = Albino.class;
-				if (cl == Rat.class)                cl = NecroRat.class;
+				else if (cl == Rat.class)                cl = NecroRat.class;
 				else if (cl == Gnoll.class)         cl = GnollExile.class;
 				else if (cl == Crab.class)          cl = HermitCrab.class;
 				else if (cl == Slime.class)         cl = CausticSlime.class;
