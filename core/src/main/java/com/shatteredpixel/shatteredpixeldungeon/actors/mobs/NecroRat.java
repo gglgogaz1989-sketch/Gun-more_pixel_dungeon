e0f4f5183bab53c4a96bdad735b8ecf1f425e53f
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 20:04:47 +0200

    Update NecroRat.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/NecroRat.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/NecroRat.java
@@ -1,52 +1,36 @@
-/*
- * Pixel Dungeon
- * Copyright (C) 2012-2015 Oleg Dolya
- *
- * Shattered Pixel Dungeon
- * Copyright (C) 2014-2025 Evan Debenham
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- */
-
 package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
 
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
-import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Bleeding;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
-import com.shatteredpixel.shatteredpixeldungeon.items.food.MysteryMeat;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Charmed;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.NecroRatSprite;
 import com.watabou.utils.Random;
 
-public class Albino extends Rat {
+public class NecroRat extends Rat {
 
-	{
-		spriteClass = NecroRatSprite.class;
-		
-		HP = HT = 15;
-		EXP = 2;
-		
-		loot = MysteryMeat.class;
-		lootChance = 1f;
-	}
-	
-	@Override
-	public int attackProc( Char enemy, int damage ) {
-		damage = super.attackProc( enemy, damage );
-		if (damage > 0 && Random.Int( 2 ) == 0) {
-			Buff.affect( enemy, Bleeding.class ).set( Random.NormalFloat(2, 3) );
-		}
-		
-		return damage;
-	}
+    {
+        // Привязываем новый класс спрайта
+        spriteClass = NecroRatSprite.class;
+
+        name = "Некро-крыса";
+        
+        HP = HT = 15; // Чуть крепче обычной крысы
+        EXP = 3;
+        
+        // Шанс уклонения и защиты как у сильного моба начальных этажей
+        defenseSkill = 3;
+    }
+
+    @Override
+    public int attackProc( Char enemy, int damage ) {
+        damage = super.attackProc( enemy, damage );
+        
+        // Если урон нанесен, с шансом 1/2 накладываем Очарование
+        if (damage > 0 && Random.Int( 2 ) == 0) {
+            // Длительность очарования от 3 до 5 ходов
+            Buff.affect( enemy, Charmed.class, objectID ).set( Random.NormalFloat(3, 5) );
+        }
+        
+        return damage;
+    }
 }
