ffb92ab953ea4d9e01e7749a48fe50a82b2fbd22
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 19:51:30 +0200

    Update Bullet.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
@@ -1,23 +1,40 @@
 package com.shatteredpixel.shatteredpixeldungeon.items;
 
+import com.watabou.utils.Bundle;
 import com.watabou.utils.Random;
 
 public class Bullet extends Item {
 
-	public Bullet() {
-		// В Shattered PD имя часто задается так:
-		image = 100;
-		stackable = true;
-	}
+    public Bullet() {
+        // Ставим ID 361 (судя по атласу, патрон в самом низу)
+        image = 361; 
+        stackable = true;
+    }
 
-	@Override
-	public Item random() {
-		quantity = Random.IntRange(30, 60);
-		return this;
-	}
+    // Эти методы ОБЯЗАТЕЛЬНЫ, чтобы игра не вылетала при переходе на другой этаж
+    @Override
+    public void storeInBundle( Bundle bundle ) {
+        super.storeInBundle( bundle );
+    }
 
-	@Override
-	public String info() {
-		return "Тяжелый золотой патрон. Стакается в инвентаре и ждет своего часа.";
-	}
+    @Override
+    public void restoreFromBundle( Bundle bundle ) {
+        super.restoreFromBundle( bundle );
+    }
+
+    @Override
+    public String toString() {
+        return "Золотой патрон";
+    }
+
+    @Override
+    public Item random() {
+        quantity = Random.IntRange(30, 60);
+        return this;
+    }
+
+    @Override
+    public String info() {
+        return "Тяжелый золотой патрон. Пока что просто копится в сумке.";
+    }
 }
