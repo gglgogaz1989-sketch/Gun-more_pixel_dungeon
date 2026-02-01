42c891a25a6e786dab5cfe2af9272bfb46c53b07
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 19:20:11 +0200

    Update Bullet.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
@@ -2,22 +2,24 @@ package com.shatteredpixel.shatteredpixeldungeon.items;
 
 import com.watabou.utils.Random;
 
+// Обязательно наследуем Item
 public class Bullet extends Item {
 
-	public Bullet() {
-		name = "Золотой патрон";
-		image = 100; // Если иконка будет "невидимой", попробуй поставить 100
-		stackable = true;
-	}
+    // Это конструктор - он ОБЯЗАТЕЛЕН
+    public Bullet() {
+        name = "Золотой патрон";
+        image = 120; 
+        stackable = true;
+    }
 
-	@Override
-	public Item random() {
-		quantity = Random.IntRange(30, 60);
-		return this;
-	}
+    @Override
+    public Item random() {
+        quantity = Random.IntRange(30, 60);
+        return this;
+    }
 
-	@Override
-	public String info() {
-		return "Тяжелый золотой патрон. Пока что просто занимает место, но скоро пригодится для стрельбы.";
-	}
+    @Override
+    public String info() {
+        return "Тяжелый золотой патрон. Стакается в инвентаре и ждет своего часа.";
+    }
 }
