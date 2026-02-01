2109213b3ff42459430c7faa1a9dd39515efccb5
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 20:24:42 +0200

    Update NecroRatSprite.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/NecroRatSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/NecroRatSprite.java
@@ -8,24 +8,21 @@ public class NecroRatSprite extends MobSprite {
 	public NecroRatSprite() {
 		super();
 		
-		// Если Assets.Sprites.RAT выдаст ошибку, поменяй на Assets.RAT
+		// Используем стандартный атлас крыс
 		texture( Assets.Sprites.RAT );
 		
 		TextureFilm frames = new TextureFilm( texture, 16, 15 );
 		
-		// Кадры покоя (48, 48, 48, 49)
+		// Кадры для 4-го ряда твоего файла
 		idle = new Animation( 2, true );
 		idle.frames( frames, 48, 48, 48, 49 );
 		
-		// Кадры бега (54, 55, 56, 57, 58)
 		run = new Animation( 10, true );
 		run.frames( frames, 54, 55, 56, 57, 58 );
 		
-		// Кадры атаки (50, 51, 52, 53, 48)
 		attack = new Animation( 15, false );
 		attack.frames( frames, 50, 51, 52, 53, 48 );
 		
-		// Кадры смерти (59, 60, 61, 62)
 		die = new Animation( 10, false );
 		die.frames( frames, 59, 60, 61, 62 );
 		
