822f8535a1b42109b5d1d82bdce5e0ee72656fbe
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-01 20:08:47 +0200

    Update NecroRat.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/NecroRat.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/NecroRat.java
@@ -2,35 +2,31 @@ package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
 
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
-import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Charmed;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Charm;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.NecroRatSprite;
 import com.watabou.utils.Random;
 
 public class NecroRat extends Rat {
 
-    {
-        // Привязываем новый класс спрайта
-        spriteClass = NecroRatSprite.class;
+	{
+		// Привязываем спрайт
+		spriteClass = NecroRatSprite.class;
 
-        name = "Некро-крыса";
-        
-        HP = HT = 15; // Чуть крепче обычной крысы
-        EXP = 3;
-        
-        // Шанс уклонения и защиты как у сильного моба начальных этажей
-        defenseSkill = 3;
-    }
+		HP = HT = 16;
+		defenseSkill = 4;
+		EXP = 3;
+	}
 
-    @Override
-    public int attackProc( Char enemy, int damage ) {
-        damage = super.attackProc( enemy, damage );
-        
-        // Если урон нанесен, с шансом 1/2 накладываем Очарование
-        if (damage > 0 && Random.Int( 2 ) == 0) {
-            // Длительность очарования от 3 до 5 ходов
-            Buff.affect( enemy, Charmed.class, objectID ).set( Random.NormalFloat(3, 5) );
-        }
-        
-        return damage;
-    }
+	@Override
+	public int attackProc( Char enemy, int damage ) {
+		int actualDamage = super.attackProc( enemy, damage );
+		
+		// Если урон > 0, с шансом 50% накладываем Charm
+		if (actualDamage > 0 && Random.Int( 2 ) == 0) {
+			// Накладываем бафф Charm на врага. Источник — эта крыса (this)
+			Buff.affect( enemy, Charm.class ).set( Random.NormalFloat(3, 5) );
+		}
+		
+		return actualDamage;
+	}
 }
