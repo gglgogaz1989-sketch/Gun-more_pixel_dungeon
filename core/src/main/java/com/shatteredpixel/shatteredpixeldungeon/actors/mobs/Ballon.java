648606735a0dcdcf8c734187383bb5af33ccf26d
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-03 16:20:43 +0200

    Update Ballon.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Ballon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/Ballon.java
@@ -1,46 +1,47 @@
 package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
 
-import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
-import com.shatteredpixel.shatteredpixeldungeon.sprites.BallSprite;
-import com.shatteredpixel.shatteredpixeldungeon.items.potions.HealingPotion;
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.BallonSprite; // Поменял на твое название!
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.HealthPotion; // В Shattered он HealthPotion, а не Healing
 import com.shatteredpixel.shatteredpixeldungeon.items.potions.ExperiencePotion;
-import com.shatteredpixel.utils.Random;
+import com.watabou.utils.Random; // В этой версии Random лежит здесь
 
 public class Ballon extends Mob {
 
+    // В новых версиях параметры задаются в конструкторе или блоке инициализации
     {
-        name = "Праздничный Шарик";
-        spriteClass = BallSprite.class;
+        spriteClass = BallonSprite.class;
 
-        HP = HT = 1; // Лопается с одного тычка
-        EXP = 0;     // Опыт даем через банки
-
-        state = PASSIVE; // Сидит смирно
-        properties.add(Property.IMMOVABLE); // Нельзя сдвинуть или заставить идти
+        hp = ht = 1; 
+        exp = 0;
+    }
+    
+    @Override
+    public String name() {
+        return "Праздничный Шарик";
     }
 
     @Override
     public boolean act() {
-        // Шарик просто висит, тратя время хода на ожидание
         spend( TICK );
         return true;
     }
 
     @Override
-    public int attackSkill(Char target) { return 0; } // Не атакует
+    public int attackSkill(com.shatteredpixel.shatteredpixeldungeon.actors.Char target) { return 0; }
 
     @Override
-    public int damageRoll() { return 0; } // Не наносит урона
+    public int damageRoll() { return 0; }
 
     @Override
     public void die(Object cause) {
         super.die(cause);
         
-        // Выпадение подарка для мамы/игрока
+        // Исправлено: Dungeon с большой буквы и правильный вызов drop
         if (Random.Int(2) == 0) {
-            dungeon.level.drop(new HealingPotion(), pos).sprite.drop();
+            Dungeon.level.drop(new HealthPotion(), pos).sprite.drop();
         } else {
-            dungeon.level.drop(new ExperiencePotion(), pos).sprite.drop();
+            Dungeon.level.drop(new ExperiencePotion(), pos).sprite.drop();
         }
     }
-                       }
+}
