462fc8f7d77a5038c2a1765c65cf3902ae4251e3
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-04 18:32:01 +0200

    Update IronShield.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/IronShield.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/IronShield.java
@@ -1,31 +1,51 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee;
 
-import com.shatteredpixel.shatteredpixeldungeon.items.Item;
-import com.shatteredpixel.shatteredpixeldungeon.items.LiquidMetal; 
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 
 public class IronShield extends MeleeWeapon {
 
-    {
-        // Если палочка мага имеет индекс 127, то твой щит по горизонтали - 128
-        image = 128; 
-        tier = 5;
-    }
+	{
+		// Используем текстуру ростового щита, как ты и хотел
+		image = ItemSpriteSheet.GREATSHIELD;
 
-    // Внутренний класс для Recipe.java
-    public static class Recipe extends com.shatteredpixel.shatteredpixeldungeon.items.Recipe.SimpleRecipe {
-        {
-            // Жидкое железо + Ростовой щит
-            inputs = new Class[]{ LiquidMetal.class, Greatshield.class };
-            inQuantity = new int[]{ 1, 1 };
-            cost = 10;
-            output = IronShield.class;
-            outQuantity = 1;
-        }
-    }
+		tier = 5;
+	}
 
-    @Override
-    public int defenseFactor(com.shatteredpixel.shatteredpixeldungeon.actors.Char owner) {
-        // Базовая защита 8 + прогрессия от уровня (лучше чем у Greatshield)
-        return 8 + 2 * buffedLvl(); 
-    }
+	@Override
+	public int max(int lvl) {
+		// Характеристики урона как у топового щита
+		return  Math.round(3f*(tier+1)) + lvl*(tier-1);
+	}
+
+	@Override
+	public int defenseFactor( Char owner ) {
+		return DRMax();
+	}
+
+	public int DRMax(){
+		return DRMax(buffedLvl());
+	}
+
+	// Повышенная защита (8 базы вместо 6 у обычного щита)
+	public int DRMax(int lvl){
+		return 8 + 2*lvl;
+	}
+
+	@Override
+	protected void duelistAbility(Hero hero, Integer target) {
+		// Способность дуэлянта (блок)
+		RoundShield.guardAbility(hero, 4+buffedLvl(), this);
+	}
+
+	@Override
+	public boolean isUpgradable() {
+		return true;
+	}
+
+	@Override
+	public int value() {
+		return 500; // Цена в магазине
+	}
 }
