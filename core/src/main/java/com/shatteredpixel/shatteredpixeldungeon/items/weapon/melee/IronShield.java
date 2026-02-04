e1c0037942dc67740fa54fefd0575ddc7f7f2724
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-04 18:39:42 +0200

    Update IronShield.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/IronShield.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/IronShield.java
@@ -7,15 +7,13 @@ import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 public class IronShield extends MeleeWeapon {
 
 	{
-		// Используем текстуру ростового щита, как ты и хотел
+		// Текстура ростового щита
 		image = ItemSpriteSheet.GREATSHIELD;
-
 		tier = 5;
 	}
 
 	@Override
 	public int max(int lvl) {
-		// Характеристики урона как у топового щита
 		return  Math.round(3f*(tier+1)) + lvl*(tier-1);
 	}
 
@@ -28,24 +26,17 @@ public class IronShield extends MeleeWeapon {
 		return DRMax(buffedLvl());
 	}
 
-	// Повышенная защита (8 базы вместо 6 у обычного щита)
 	public int DRMax(int lvl){
 		return 8 + 2*lvl;
 	}
 
 	@Override
 	protected void duelistAbility(Hero hero, Integer target) {
-		// Способность дуэлянта (блок)
 		RoundShield.guardAbility(hero, 4+buffedLvl(), this);
 	}
 
 	@Override
-	public boolean isUpgradable() {
-		return true;
-	}
-
-	@Override
 	public int value() {
-		return 500; // Цена в магазине
+		return 500;
 	}
 }
