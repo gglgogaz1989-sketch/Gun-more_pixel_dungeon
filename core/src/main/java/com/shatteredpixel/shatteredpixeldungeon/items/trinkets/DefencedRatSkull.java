7e4d386973ceba8ef3b19adcb5c5fc697ad9d14f
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-06 12:47:26 +0200

    Update DefencedRatSkull.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/trinkets/DefencedRatSkull.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/trinkets/DefencedRatSkull.java
@@ -27,7 +27,7 @@ import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 public class DefencedRatSkull extends Trinket {
 
 	{
-		image = ItemSpriteSheet.DEFENCED_RAT_SKULL;
+		image = ItemSpriteSheet.RAT_SKULL;
 	}
 
 	@Override
