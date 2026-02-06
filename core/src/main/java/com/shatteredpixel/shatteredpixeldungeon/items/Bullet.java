d3093998a38adeaee9c29adefbbb50d6ab6c15eb
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-06 11:36:36 +0200

    Update Bullet.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
@@ -1,14 +1,15 @@
 package com.shatteredpixel.shatteredpixeldungeon.items;
 
-import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import java.util.ArrayList;
 
 public class Bullet extends Item {
 
     public Bullet() {
         super();
-        // Ставим 16, как ты и сказал
+        // Номер текстуры из твоего файла items.png
         image = 16; 
-        stackable = true;
+        stackable = true; // Пули собираются в пачки
     }
 
     @Override
@@ -18,6 +19,27 @@ public class Bullet extends Item {
 
     @Override
     public String desc() {
-        return "Металлический снаряд. Выглядит опасно.";
+        return "Тяжелый металлический снаряд. В руках он не очень полезен, но если его запустить из чего-то мощного...";
+    }
+
+    // Добавляем действия для пули
+    @Override
+    public ArrayList<String> actions(Hero hero) {
+        ArrayList<String> actions = super.actions(hero);
+        // Пулю нельзя надеть, но её можно БРОСИТЬ (Throw)
+        actions.add(AC_THROW);
+        return actions;
+    }
+
+    @Override
+    public void execute(Hero hero, String action) {
+        // Если игрок нажал "Бросить"
+        if (action.equals(AC_THROW)) {
+            // Запускаем стандартную механику броска предмета
+            curUser = hero;
+            // После этого кода игра предложит выбрать цель для броска
+        } else {
+            super.execute(hero, action);
+        }
     }
 }
