c3f96ba97dedab8bbcba67d666c2f9b24aef2635
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-06 11:31:58 +0200

    Update IronShield.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/IronShield.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/IronShield.java
@@ -1,42 +1,35 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee;
 
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
-import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
-import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.MeleeWeapon;
 
 public class IronShield extends MeleeWeapon {
 
-	{
-		// Текстура ростового щита
-		image = ItemSpriteSheet.GREATSHIELD;
-		tier = 5;
-	}
+    public IronShield() {
+        tier = 5;
+        // ВАЖНО: Поменяй 17 на номер ячейки, где у тебя нарисован ЩИТ
+        image = 17; 
+    }
 
-	@Override
-	public int max(int lvl) {
-		return  Math.round(3f*(tier+1)) + lvl*(tier-1);
-	}
+    @Override
+    public String name() {
+        return "Железный щит";
+    }
 
-	@Override
-	public int defenseFactor( Char owner ) {
-		return DRMax();
-	}
+    @Override
+    public String desc() {
+        return "Тяжелый железный щит. Его практически невозможно пробить, но он очень тяжелый.";
+    }
 
-	public int DRMax(){
-		return DRMax(buffedLvl());
-	}
-
-	public int DRMax(int lvl){
-		return 8 + 2*lvl;
-	}
-
-	@Override
-	protected void duelistAbility(Hero hero, Integer target) {
-		RoundShield.guardAbility(hero, 4+buffedLvl(), this);
-	}
-
-	@Override
-	public int value() {
-		return 500;
-	}
+    // ЭТОТ МЕТОД ЗАСТАВЛЯЕТ ЩИТ БЛОКИРОВАТЬ
+    @Override
+    public int defenseRoll(Char user) {
+        // Блокирует от 0 до (10 + уровень заточки * 2)
+        return 10 + (level * 4);
+    }
+    
+    @Override
+    public int minStrength() {
+        return 14 - level;
+    }
 }
