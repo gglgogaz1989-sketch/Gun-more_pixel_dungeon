8ad12c9c54e4b69ee7b9918b4900a977b9f4f835
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-06 13:57:30 +0200

    Update IronShield.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/IronShield.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/IronShield.java
@@ -5,20 +5,26 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 public class IronShield extends MeleeWeapon {
 
     {
-        // Текстура на координатах 96x96
+        // Индекс для координат 96x96
         image = 102; 
         tier = 5;
     }
 
+    // Минимальный урон
     @Override
-    public int max(int lvl) {
-        return Math.round(3f*(tier+1)) + lvl*(tier-1);
+    public int min(int lvl) {
+        return 40 + (lvl * 2); // 40 базовый + 2 за каждый уровень заточки
     }
 
-    // Метод блокировки урона
+    // Максимальный урон
+    @Override
+    public int max(int lvl) {
+        return 60 + (lvl * 5); // 60 базовый + 5 за каждый уровень заточки
+    }
+
     @Override
     public int defenseFactor(Char owner) {
-        // 10 базовой защиты + 4 за уровень улучшения
+        // Блок: 10 базовой защиты + 4 за уровень заточки
         return 10 + 4 * buffedLvl();
     }
 
@@ -29,6 +35,6 @@ public class IronShield extends MeleeWeapon {
 
     @Override
     public String desc() {
-        return "Огромный щит из литого железа. Отлично блокирует удары.";
+        return "Огромный щит из литого железа. Им можно не только защищаться, но и больно бить.";
     }
 }
