c403bfc3c05cfe5fac52ab86404498b69f023132
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-06 11:01:54 +0200

    Update Bullet.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
@@ -1,37 +1,25 @@
-package com.shatteredpixel.shatteredpixeldungeon.items.weapon.missiles;
+package com.shatteredpixel.shatteredpixeldungeon.items; // ИСПРАВЛЕНО: теперь адрес верный!
 
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 
 public class Bullet extends Item {
 
-    // КОНСТРУКТОР (Здесь мы безопасно задаем параметры)
     public Bullet() {
-        super(); // Обязательная строка
-        
-        // Прямое присваивание имени часто вызывает ошибки в новых версиях,
-        // но внутри конструктора это обычно работает.
-        // Если вдруг снова будет ошибка - удали строку с name ниже.
-        this.name = "Пуля"; 
-        
-        // Индекс 15 - это та самая пуля рядом с камнем в твоем файле items.png
-        this.image = 15; 
-        
-        // Делаем так, чтобы пули собирались в кучу (стакались)
-        this.stackable = true;
+        super();
+        // ИСПРАВЛЕНО: image указываем здесь
+        image = 15; 
+        stackable = true;
     }
 
-    @Override
-    public String desc() {
-        // Описание предмета
-        return "Тяжелый металлический снаряд для дальнего боя.";
-    }
-
-    // Если ошибка с name все равно останется, раскомментируй (убери //) код ниже,
-    // а строку 'this.name = ...' сверху удали.
-    /*
+    // ИСПРАВЛЕНО: Вместо this.name = ... используем этот метод
     @Override
     public String name() {
         return "Пуля";
     }
-    */
+
+    @Override
+    public String desc() {
+        return "Тяжелый металлический снаряд для дальнего боя.";
+    }
 }
