91b01947f045f145492353ac014ab70e725e36e1
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-06 13:55:05 +0200

    Update Bullet.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/Bullet.java
@@ -1,12 +1,9 @@
 package com.shatteredpixel.shatteredpixeldungeon.items;
 
-import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
-import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
-
 public class Bullet extends Item {
 
     {
-        // Индекс для координат 209x17
+        // Текстура на координатах 209x17
         image = 29; 
         stackable = true;
     }
@@ -16,14 +13,11 @@ public class Bullet extends Item {
         return "Пуля";
     }
 
-    // Для обычных предметов (Item) урон при броске задается этим методом
-    @Override
-    public int thrownDamage(Hero hero, Char target) {
-        return Hero.heroDamageIntRange(10, 20);
-    }
-
     @Override
     public String desc() {
-        return "Тяжелый металлический снаряд. Наносит 10-20 урона при броске.";
+        return "Тяжелый металлический снаряд. Пока что его можно только бросить, но в будущем он пригодится для огнестрельного оружия.";
     }
+    
+    // Мы убрали thrownDamage и damageRoll, чтобы избежать ошибок компиляции.
+    // Теперь это просто стакающийся предмет.
 }
