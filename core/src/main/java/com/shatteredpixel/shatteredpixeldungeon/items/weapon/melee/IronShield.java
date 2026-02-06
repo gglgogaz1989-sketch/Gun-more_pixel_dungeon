f3d5415ab72473c8cfcd3386e63b57489c6dd0ff
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-06 13:50:36 +0200

    Update IronShield.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/IronShield.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/IronShield.java
@@ -1,12 +1,11 @@
 package com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee;
 
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
-import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
 
 public class IronShield extends MeleeWeapon {
 
     {
-        // Индекс для координат 96x96
+        // Текстура на координатах 96x96
         image = 102; 
         tier = 5;
     }
@@ -16,10 +15,10 @@ public class IronShield extends MeleeWeapon {
         return Math.round(3f*(tier+1)) + lvl*(tier-1);
     }
 
-    // ВАЖНО: Метод для блокировки в Shattered PD
+    // Метод блокировки урона
     @Override
     public int defenseFactor(Char owner) {
-        // Возвращаем значение блока (10 базовый + 4 за уровень)
+        // 10 базовой защиты + 4 за уровень улучшения
         return 10 + 4 * buffedLvl();
     }
 
@@ -27,10 +26,9 @@ public class IronShield extends MeleeWeapon {
     public String name() {
         return "Железный щит";
     }
-    
-    // Добавим этот метод для надежности, если версия игры чуть старее
+
     @Override
-    public int dr() {
-        return 10 + 4 * buffedLvl();
+    public String desc() {
+        return "Огромный щит из литого железа. Отлично блокирует удары.";
     }
 }
