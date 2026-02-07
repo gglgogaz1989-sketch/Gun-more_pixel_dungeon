79b9ec8b66a27747196f56c51300d2f685586e02
gglgogaz1989-sketch <gglgogaz1989@gmail.com>
2026-02-07 19:51:52 +0200

    Update Hero.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Hero.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/Hero.java
@@ -243,7 +243,7 @@ public class Hero extends Char {
 	public Hero() {
 		super();
 
-		HP = HT = 50;
+		HP = HT = 25;
 		STR = STARTING_STR;
 		
 		belongings = new Belongings( this );
@@ -254,7 +254,7 @@ public class Hero extends Char {
 	public void updateHT( boolean boostHP ){
 		int curHT = HT;
 		
-		HT = 20 + 5*(lvl-1) + HTBoost;
+		HT = 25 + 5*(lvl-1) + HTBoost;
 		float multiplier = RingOfMight.HTMultiplier(this);
 		HT = Math.round(multiplier * HT);
 		
